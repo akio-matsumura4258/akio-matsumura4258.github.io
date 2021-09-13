8c656fb324d8f74d206a325b52cd0174c122f68a
Akio Matsumura <81938114+akio-matsumura4258@users.noreply.github.com>
2021-09-13 12:12:45 +0900

    Add files via upload

---
+++ AvatarRobot/basic2016/signaling.js
@@ -0,0 +1,31 @@
+"use strict";
+ 
+let WebSocketServer = require('ws').Server;
+let port = 3001;
+let wsServer = new WebSocketServer({ port: port });
+console.log('websocket server start. port=' + port);
+ 
+wsServer.on('connection', function(ws) {
+  console.log('-- websocket connected --');
+  ws.on('message', function(message) {
+
+	// デバッグ用　ちゃんとJSONを受信しているか？表示する
+    let obj = JSON.parse(message);
+  	console.log(obj);
+
+    wsServer.clients.forEach(function each(client) {
+      if (isSame(ws, client)) {
+        console.log('- skip sender -');
+      }
+      else {
+        console.log('- client send -');
+        client.send(message);
+      }
+    });
+  });
+});
+ 
+function isSame(ws1, ws2) {
+  // -- compare object --
+  return (ws1 === ws2);     
+}
